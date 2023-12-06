70fe6115a28865e89598ccf2409e71b72c125936
phyerim <aamm2222@hanmail.net>
2023-12-07 05:17:05 +0900

    ...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-// import './swiper.css'
+import './swiper.css'
 import ps from './resource/photoshop.png'
 import ai from './resource/ai.png'
 import clip from './resource/clip.png'
@@ -9,6 +9,11 @@ import html from './resource/html.png'
 import excel from './resource/excel.png'
 import js from './resource/js.png'
 import pro from './resource/pro.png'
+import c1 from './resource/c1.svg'
+import c2 from './resource/c2.svg'
+import c3 from './resource/c3.svg'
+import c4 from './resource/c4.svg'
+import c5 from './resource/c5.svg'
 import port1 from './resource/port1.jpg'
 import port2 from './resource/port2.jpg'
 import port3 from './resource/port3.jpg'
@@ -24,7 +29,6 @@ import nav from './resource/nav.svg';
 import cancel from './resource/cancel.svg';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, {Navigation,Pagination,Scrollbar,Mousewheel,A11y} from "swiper"
-
 const App =  (()=> {
 
   SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Mousewheel]); // *
@@ -57,9 +61,23 @@ const App =  (()=> {
     // {title:'도담헤어 로고 제작',content:'미용실 도담헤어 로고 디자인',tool:[ps],link:[],img:port6},
     // {title:'곱빼기 로고 제작',content:'서산시 배달앱 곱빼기 로고 디자인',tool:[ai],link:[],img:port7},
   ]
+
+  const chList =[
+    {title:'불량이',content:'불만고양이 불량이',personality:['매사에 불만이 많음','짠돌이 고양이','잔소리꾼'],color:['#5d5d5d','#fff','#ff9b9f'],img:c1},
+    {title:'즐량이',content:'즐거운 고양이 즐량이',personality:['항상 즐거움','알콜러버! 술을 사랑함','흥이 많음'],color:['#ffcccd','#fff','#ff9b9f',],img:c2},
+   
+    {title:'겁량이',content:'겁쟁이 고양이 겁량이',personality:['항상 겁에 질려있음','낯을 많이 가림','노래 부르기를 좋아함'],color:['#ffe1a0','#fff','#ff9b9f',],img:c3},
+    {title:'식댕이',content:'식빵 닮은 멍멍이 식댕이',personality:['항상 겁에 질려있음','낯을 많이 가림','노래 부르기를 좋아함'],color:['#fff3e6','#c89264','#ff3636',],img:c4},
+    {title:'주달이',content:'술을 사랑하는 수달',personality:['항상 겁에 질려있음','낯을 많이 가림','노래 부르기를 좋아함'],color:['#a0dae8','#ff986c','#ff9ca6','#fff',],img:c5},
+    
+  ]
+
+
   const [pageNumber,setPageNumber] = useState(0)
   const [swiper,setSwiper] = useState(null)
   const [portNumber,setPortNumber] = useState(0)
+  
+  const [chNumber,setChNumber] = useState(0)
   const [navBar,setNavBar] = useState(false)
   const handlePortfolio = (lf) => {
     let number = portNumber
@@ -81,10 +99,28 @@ const App =  (()=> {
     
     setPortNumber(number)
   }
+  
+  const handleCh = (lf) => {
+    let number = chNumber
+    if(lf==='left'){
+      if(number===0){
+        number = 4;   
+      }else{
+        number = number - 1;
+      }
+    }else{
+      if(number===4){
+       
+        number = 0; 
+      }else{
 
-  useEffect(()=>{
-    console.log(portNumber)
-  },[portNumber])
+        number = number + 1;
+      }
+    }
+    
+    setChNumber(number)
+  }
+ 
  
    
   return (
@@ -127,7 +163,7 @@ const App =  (()=> {
           initialSlide={0}                          
           loop={false}
           loopedSlides={1}
-          observer={true}
+          // observer={true}
           observeParents={true}
           resistance={true}>
 
@@ -174,6 +210,28 @@ const App =  (()=> {
                   </div>
                   <div className='carrersLayout'>
                     <div className='carrersDate'>
+                      2023.10 ~
+                    </div>
+                    <div className='carrersDate2'>
+                      2012.10 ~<br/>
+                    </div> 
+                    <div className='carrersLine'>
+                      <div className='circle'/>
+                      <div className='line'/>
+                    </div>
+                    
+                    <div className='carrersContents'>
+                      <span>
+                        미리캔버스 기여자 활동
+                      </span>
+                      <div>
+                        요소 디자인
+                      </div>
+                    </div>
+                  </div> 
+
+                  <div className='carrersLayout'>
+                    <div className='carrersDate'>
                       2020.02 ~ 2023.08
                     </div>
                     <div className='carrersDate2'>
@@ -205,33 +263,14 @@ const App =  (()=> {
                     </div>
                     <div className='carrersContents'>
                       <span>
-                        온라인 중고서점 우리아이책더미
+                        우리아이책더미 운영
                       </span>
                       <div>
-                        yes24/알라딘 상품등록, 관리
+                        오픈마켓 상품 등록, 관리
                       </div>
                     </div>
                   </div>
-                  <div className='carrersLayout'>
-                    <div className='carrersDate'>
-                      2012.08 ~ 2013.04
-                    </div>
-                    <div className='carrersDate2'>
-                      2012.08 ~<br/>2013.04
-                    </div>
-                    <div className='carrersLine'>
-                      <div className='circle'/>
-                      <div className='line'/>
-                    </div>
-                    <div className='carrersContents'>
-                      <span>
-                        신원해피니스
-                      </span>
-                      <div>
-                        디자인
-                      </div>
-                    </div>
-                  </div> 
+              
                   <div className='lastCarrers'>
                     <span/>
                   </div>
@@ -290,7 +329,7 @@ const App =  (()=> {
                 <div className='aboutLayout' style={{paddingTop:70}}> 
                   <div className='aboutTitle' style={{marginBottom:50}}>
                     <span/>
-                    PORTFOLIO
+                    APP / WEB SITE
                   </div>
                   <div className='portfolioLayout'>
                       <img onClick={()=>handlePortfolio('left')} src={back} className='arrowLeft' alt=''/>
@@ -327,6 +366,53 @@ const App =  (()=> {
                       </div>
                     )}
                   </div>
+                </div>
+                <div className='electronicDisplay'>
+                  PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO 
+                </div>
+              </div>  
+            </SwiperSlide> 
+
+            <SwiperSlide >  
+              <div className='slide'>
+                <div className='aboutLayout' style={{paddingTop:70}}> 
+                  <div className='aboutTitle' style={{marginBottom:50}}>
+                    <span/>
+                    CHARACTER
+                  </div>
+                  <div className='portfolioLayout'>
+                      <img onClick={()=>handleCh('left')} src={back} className='arrowLeft' alt=''/>
+                      <div className='characterItem'>
+                        <img src={chList[chNumber].img} alt='' className='portImg'/>
+                        <div className='portContents'>
+                          <div className='portTitle'>{chList[chNumber].title}</div>
+                          <div className='portContent'>{chList[chNumber].content}</div>
+                          <div className='chColor'>
+                            {chList[chNumber].color.map((vval,vidx)=>
+                              <div key={vidx} style={{backgroundColor:vval}}/>
+                            )} 
+                          </div>
+                          <div>
+                            {chList[chNumber].personality.map((vvval,vvidx)=>
+                              <div key={vvidx}>
+                                {vvval}
+                              </div>
+                            )}
+                          </div>
+                          
+                        </div>
+                      </div>
+                    <img onClick={()=>handleCh('right')} src={next} className='arrowRight' alt=''/>
+                    <div className='portPaddingBox'/>
+                  </div>
+                  <div className='portButtonBar'>
+                    {chList.map((val,idx)=>
+                      <div className='portButtonItem' onClick={()=>setChNumber(idx)}>
+                        <div className={idx===chNumber?'portOn':'portOff'}/>
+                      </div>
+                    )}
+                  </div>
+                 
                 </div>
                 <div className='electronicDisplay'>
                   PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO PORTFOLIO 
